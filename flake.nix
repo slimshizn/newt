35d82ea15c3a416205c26782c56ca422817991d3
Scott Hansen <tech@firecat53.net>
2025-06-06 07:36:48 -0700

    Update flake.nix for 1.2.1

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
           default = self.packages.${system}.pangolin-newt;
           pangolin-newt = pkgs.buildGoModule {
             pname = "pangolin-newt";
-            version = "1.2.0";
+            version = "1.2.1";
 
             src = ./.;
 
